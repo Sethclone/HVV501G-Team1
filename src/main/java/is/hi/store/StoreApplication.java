8d405144019be5cbe6c666b7a6d518b5d32e3093
Aron Ingi <aronikjartansson@gmail.com>
2026-09-11 16:21:01 +0000

    Refactoring

--- src/main/java/is/hi/store/StoreApplication.java
+++ src/main/java/is/hi/store/StoreApplication.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import is.hi.store.entity.User;
+import is.hi.store.repository.UserRepository;
 
 @SpringBootApplication
 @RestController
--- src/main/java/is/hi/store/entity/User.java
+++ src/main/java/is/hi/store/entity/User.java
@@ -1,7 +1,5 @@
-package is.hi.store;
-
+package is.hi.store.entity;
 import jakarta.persistence.*;
-import org.springframework.data.repository.Repository;
 
 @Entity
 @Table(name="users")
@@ -19,10 +17,4 @@ public class User {
 	public void setName(String name) {this.name=name;}
 	public String getName() {return name;}
 	public long getId() {return id;}
-}
-
-interface UserRepository extends Repository<User, Long> {
-	User save(User user);
-	User findById(long id);
-	Long count();
 }
